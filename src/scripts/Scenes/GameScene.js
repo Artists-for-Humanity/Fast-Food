595438d5f1471dace92a5ebd681e7c6e37201ba7
TunaYou <lolboyrl7@gmail.com>
2022-02-17 16:53:49 -0500

    changessss

--- src/scripts/Scenes/GameScene.js
+++ src/scripts/Scenes/GameScene.js
@@ -3,6 +3,7 @@ import Customer from '../Sprites/Customer';
 import Line from '../Sprites/Line';
 import Player from '../Sprites/Player';
 import Counter from '../Sprites/Counter';
+import Food from '../Sprites/Food';
 
 export default class GameScene extends Phaser.Scene {
   player;
@@ -28,6 +29,8 @@ export default class GameScene extends Phaser.Scene {
     this.load.image('person6', new URL('../../assets/person6.png', import.meta.url).href);
     this.load.image('person7', new URL('../../assets/person7.png', import.meta.url).href);
     this.load.image('person8', new URL('../../assets/person8.png', import.meta.url).href);
+    this.load.image('food', new URL('../../assets/burger.png', import.meta.url).href);
+
   }
 
   create() {
@@ -35,6 +38,7 @@ export default class GameScene extends Phaser.Scene {
     this.player = new Player(this, this.game.config.width / 2, this.game.config.height / 2);
     this.line = new Line(this, this.game.config.width / 2, this.game.config.height / 2);
     this.createSpawnZone();
+    this.burger1 = new Food(this, this.game.config.width / 2, this.game.config.height / 2);
 
     const customerPositions = [];
     for (let i = 1; i < 9; i++) {
@@ -61,6 +65,7 @@ export default class GameScene extends Phaser.Scene {
     this.line.update();
     this.customers.map((customer) => {
       customer.update();
+      this.burger1.update();
     });
   }
 
