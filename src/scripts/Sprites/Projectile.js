95f800817225b3dcdb3612df9260a001e8cef8d7
TunaYou <lolboyrl7@gmail.com>
2022-03-10 15:04:02 -0500

    added sprite collisions

--- src/scripts/Sprites/Projectile.js
+++ src/scripts/Sprites/Projectile.js
@@ -1,47 +1,39 @@
-i
-
-export default class Projectile extends Phaser.Physics.Arcade.Sprite  {
+export default class Projectile extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
         super(scene, x, y, 'food1');
-    }
-    fire(x,y,angle) {
-        this.body.reset(x,y);
-        this.setActive(true);
-        this.setVisible(true);
-        this.arcade = new Phaser.Physics.Arcade.ArcadePhysics(this.scene);
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        // this.setActive(true);
+        // this.setVisible(true);
         this.setScale(0.1);
-        // const angleVec = this.arcade.velocityFromAngle(angle)
-        // console.log(angleVec)
+    }
+    fire(angle) {
+        this.arcade = new Phaser.Physics.Arcade.ArcadePhysics(this.scene);
         const vel = this.scene.physics.velocityFromRotation(angle - Phaser.Math.DegToRad(90), 300);
-        // console.log(angle, vel);
         this.setVelocity(vel.x, vel.y);
-        
     }
 }
 
+// export default class LaserGroup extends Phaser.Physics.Arcade.Group
+// {
+//     constructor(scene) {
+//         super(scene.physics.world, scene);
 
+//         // const key = []; 
 
+//         this.createMultiple({
+//             classType: Projectile,
+//             frameQuantity: 300,
+//             active: false,
+//             visible: false,
+//             key: 'player'
 
-export default class LaserGroup extends Phaser.Physics.Arcade.Group
-{
-    constructor(scene) {
-        super(scene.physics.world, scene);
-
-        // const key = []; 
-
-        this.createMultiple({
-            classType: Projectile,
-            frameQuantity: 300,
-            active: false,
-            visible: false,
-            key: 'player'
-
-        })
-    }
-    fireLaser(x, y, angle) {
-        const projec = this.getFirstDead(false);
-        if(projec) {
-            projec.fire(x,y,angle);
-        }
-    }
-}
+//         })
+//     }
+//     fireLaser(x, y, angle) {
+//         const projec = this.getFirstDead(false);
+//         if(projec) {
+//             projec.fire(x,y,angle);
+//         }
+//     }
+// }
