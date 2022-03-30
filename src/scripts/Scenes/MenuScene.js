fec2457546349103faff7076066bf1f26f6a4bb4
Jamie De Los Santos <77027304+itisjamied@users.noreply.github.com>
2022-03-30 17:11:12 -0400

    Menu scene Edits
     -changed menu image to logo

--- src/scripts/Scenes/MenuScene.js
+++ src/scripts/Scenes/MenuScene.js
@@ -9,33 +9,36 @@ export default class MenuScene extends Phaser.Scene {
 
   preload() {
     this.load.image('menu-scene', new URL('../../assets/burger.png', import.meta.url).href);
+    this.load.image('logo', new URL('../../assets/logo.svg', import.meta.url).href);
+    this.load.image('directions', new URL('../../assets/menuDirections.svg', import.meta.url).href);
   }
 
   create() {
-    this.add.image(this.game.config.width / 2, this.game.config.height / 2, 'menu-scene');
+    this.add.image(this.game.config.width / 2, this.game.config.height - 500, 'logo');
+    this.add.image(this.game.config.width / 2, this.game.config.height - 150 , 'directions');
 
-    WebFont.load({
-      custom: {
-        families: ['Play'],
-      },
-      active: () => {
-        this.add
-          .text(
-            this.game.config.width / 2,
-            this.game.config.height - 50,
-            'PRESS SPACEBAR TO START',
-            {
-              fontFamily: 'Play',
-              fontSize: '50px',
-              fill: colors.black,
-              align: 'center',
-              backgroundColor: 'white',
-              shadowColor: 'red',
-            }
-          )
-          .setOrigin(0.5);
-      },
-    });
+    // WebFont.load({
+    //   custom: {
+    //     families: ['Play'],
+    //   },
+    //   active: () => {
+    //     this.add
+    //       .text(
+    //         this.game.config.width / 2,
+    //         this.game.config.height - 50,
+    //         'PRESS SPACEBAR TO START',
+    //         {
+    //           fontFamily: 'Play',
+    //           fontSize: '50px',
+    //           fill: colors.black,
+    //           align: 'center',
+    //           backgroundColor: 'white',
+    //           shadowColor: 'red',
+    //         }
+    //       )
+    //       .setOrigin(0.5);
+    //   },
+    // });
 
     // Stops scrolling when you click spacebar
     this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
