80e7e0f65719d75f0c7d93963198b5d795f3cd99
Stephen Cronin <cronin4392@gmail.com>
2022-05-17 17:35:57 -0400

    Add docs back

---
+++ docs/scripts/Scenes/MenuScene.js
@@ -0,0 +1,53 @@
+import * as __SNOWPACK_ENV__ from '../../snowpack/env.js';
+import.meta.env = __SNOWPACK_ENV__;
+
+import Phaser from '../../snowpack/pkg/phaser.js';
+import WebFont from '../../snowpack/pkg/webfontloader.js';
+import { colors } from '../constants.js';
+
+export default class MenuScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'MenuScene' });
+  }
+
+  preload() {
+    this.load.image('menu-scene', new URL('../../assets/burger.png', import.meta.url).href);
+    this.load.image('logo', new URL('../../assets/logo.svg', import.meta.url).href);
+    this.load.image('directions', new URL('../../assets/menuDirections.svg', import.meta.url).href);
+  }
+
+  create() {
+    this.add.image(this.game.config.width / 2, this.game.config.height - 500, 'logo');
+    this.add.image(this.game.config.width / 2, this.game.config.height - 150 , 'directions');
+
+    // WebFont.load({
+    //   custom: {
+    //     families: ['Play'],
+    //   },
+    //   active: () => {
+    //     this.add
+    //       .text(
+    //         this.game.config.width / 2,
+    //         this.game.config.height - 50,
+    //         'PRESS SPACEBAR TO START',
+    //         {
+    //           fontFamily: 'Play',
+    //           fontSize: '50px',
+    //           fill: colors.black,
+    //           align: 'center',
+    //           backgroundColor: 'white',
+    //           shadowColor: 'red',
+    //         }
+    //       )
+    //       .setOrigin(0.5);
+    //   },
+    // });
+
+    // Stops scrolling when you click spacebar
+    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    this.input.keyboard.on('keydown-SPACE', (event) => {
+      this.scene.start('GameScene');
+    });
+  }
+}
---
+++ docs/scripts/constants.js
@@ -0,0 +1,8 @@
+const colors = {
+  white: '#ffffff',
+  black: '#000000',
+  red: '#F82323',
+  green: '#00FF00'
+};
+
+export { colors };
